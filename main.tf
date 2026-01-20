59e4304ff4860403db56444bf5cbfec466ffa4f5
Muhammed Ridvan Gider <mridvang@gmail.com>
2026-01-20 13:28:55 +0300

    parametrized the location

--- main.tf
+++ main.tf
@@ -10,10 +10,6 @@ terraform {
 }
 
 provider "azurerm" {
-  # client_id       = var.azure_client_id
-  # client_secret   = var.azure_client_secret
-  # tenant_id       = var.azure_tenant_id
-  # subscription_id = var.azure_subscription_id
   features {}
 }
 
@@ -21,7 +17,7 @@ resource "azurerm_resource_group" "bookshelf" {
   tags = {
     project = "bookshelf"
   }
-  location = "switzerlandnorth"
+  location = var.location
   name     = "bookshelf"
 }
 
--- variables.tf
+++ variables.tf
@@ -1,20 +1,5 @@
-# variable "azure_client_id" {
-#   type        = string                              # The type of the variable, in this case a string
-#   description = "Azure Service Principle Client Id" # Description of what this variable represents
-# }
-
-# variable "azure_client_secret" {
-#   type        = string                                  # The type of the variable, in this case a string
-#   description = "Azure Service Principle Client Secret" # Description of what this variable represents
-#   sensitive   = true
-# }
-
-# variable "azure_tenant_id" {
-#   type        = string            # The type of the variable, in this case a string
-#   description = "Azure Tenant Id" # Description of what this variable represents
-# }
-
-# variable "azure_subscription_id" {
-#   type        = string                  # The type of the variable, in this case a string
-#   description = "Azure Subscription Id" # Description of what this variable represents
-# }
+variable "location" {
+  type = string
+  default = "switzerlandnorth"
+  description = "Location of the resources"
+}
