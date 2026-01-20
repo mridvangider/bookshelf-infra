7586424100a187108dc6a516d4200b0317587a55
Muhammed Ridvan Gider <mridvang@gmail.com>
2026-01-20 13:22:53 +0300

    removed variables

--- main.tf
+++ main.tf
@@ -10,10 +10,10 @@ terraform {
 }
 
 provider "azurerm" {
-  client_id       = var.azure_client_id
-  client_secret   = var.azure_client_secret
-  tenant_id       = var.azure_tenant_id
-  subscription_id = var.azure_subscription_id
+  # client_id       = var.azure_client_id
+  # client_secret   = var.azure_client_secret
+  # tenant_id       = var.azure_tenant_id
+  # subscription_id = var.azure_subscription_id
   features {}
 }
 
