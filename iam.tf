04804c00ce2c03e7ca7b6bb6b81c1b0a487e67d6
Muhammed Ridvan Gider <mridvang@gmail.com>
2026-01-27 09:54:44 +0300

    separated authorization parts into a file

---
+++ iam.tf
@@ -0,0 +1,18 @@
+resource "azurerm_user_assigned_identity" "app_mi" {
+  name                = "bookshelf-app-mi"
+  resource_group_name = azurerm_resource_group.bookshelf.name
+  location            = azurerm_resource_group.bookshelf.location
+}
+
+resource "azurerm_role_assignment" "bookshelf_cr_pull" {
+  scope                = data.azurerm_container_registry.main_acr.id
+  role_definition_name = "AcrPull"
+  principal_id         = azurerm_user_assigned_identity.app_mi.principal_id
+}
+
+resource "azurerm_role_assignment" "bookshelf_kv_access" {
+  scope                = data.azurerm_key_vault.infra_key_vault.id
+  role_definition_name = "Key Vault Secrets User"
+  principal_id         = azurerm_user_assigned_identity.app_mi.principal_id
+
+}
