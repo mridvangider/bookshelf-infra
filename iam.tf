ab9e9b3023cc077603fe18ac134461fce30466ff
Ridvan Gider <ridvan.gider@adesso.com.tr>
2026-01-28 15:24:50 +0300

    added container registry reader role

--- iam.tf
+++ iam.tf
@@ -16,6 +16,12 @@ resource "azurerm_role_assignment" "bookshelf_cr_read" {
   principal_id         = azurerm_user_assigned_identity.app_mi.principal_id
 }
 
+resource "azurerm_role_assignment" "bookshelf_cr_repo_read" {
+  scope                = data.azurerm_container_registry.main_acr.id
+  role_definition_name = "Container Registry Repository Reader"
+  principal_id         = azurerm_user_assigned_identity.app_mi.principal_id
+}
+
 resource "azurerm_role_assignment" "bookshelf_kv_access" {
   scope                = data.azurerm_key_vault.infra_key_vault.id
   role_definition_name = "Key Vault Secrets User"
