21073cf4cf05d8672b3beac109e00d54f5b8fb34
Ridvan Gider <ridvan.gider@adesso.com.tr>
2026-02-02 12:27:01 +0300

    fixed vm rg name

--- test_vm.tf
+++ test_vm.tf
@@ -13,7 +13,7 @@ resource "azurerm_linux_virtual_machine" "testvm" {
   location              = azurerm_resource_group.bookshelf.location
   name                  = "testvm"
   network_interface_ids = [azurerm_network_interface.test_vm_nic.id]
-  resource_group_name   = azurerm_resource_group.bookshelf.location
+  resource_group_name   = azurerm_resource_group.bookshelf.name
   size                  = "Standard_A1_v2"
   admin_username        = "adminuser"
 
