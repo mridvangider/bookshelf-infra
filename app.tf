bd3b1be0a36285724d7fa3177a241cff4acd67b8
Ridvan Gider <ridvan.gider@adesso.com.tr>
2026-02-02 10:35:35 +0300

    added client_id to app settings

--- app.tf
+++ app.tf
@@ -30,5 +30,6 @@ resource "azurerm_linux_web_app" "bookshelf_app" {
   app_settings = {
     "KEYVAULT_URL"   = data.azurerm_key_vault.infra_key_vault.vault_uri
     "BOOKSHELF_PORT" = tostring(var.application_port)
+    "AZURE_CLIENT_ID" = azurerm_user_assigned_identity.app_mi.client_id
   }
 }
