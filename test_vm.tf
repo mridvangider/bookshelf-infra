eaba17fc90250b0dd6d891bd749a4bdc2c3d598f
Ridvan Gider <ridvan.gider@adesso.com.tr>
2026-02-02 12:30:50 +0300

    changed vm size

--- test_vm.tf
+++ test_vm.tf
@@ -14,7 +14,7 @@ resource "azurerm_linux_virtual_machine" "testvm" {
   name                  = "testvm"
   network_interface_ids = [azurerm_network_interface.test_vm_nic.id]
   resource_group_name   = azurerm_resource_group.bookshelf.name
-  size                  = "Standard_A1_v2"
+  size                  = "Standard_D2_v4"
   admin_username        = "adminuser"
 
   admin_ssh_key {
