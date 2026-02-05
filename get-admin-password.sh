1ca173c0161fae3852caef87e0d0b52b1027d668
Ridvan Gider <ridvan.gider@adesso.com.tr>
2026-02-05 15:24:26 +0300

    added tfvars related steps to ci/cd pipeline

---
+++ get-admin-password.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+# shellcheck disable=SC1090
+
+output_file=$1
+key_vault_env_file=$2
+
+if [ ! -f "$key_vault_env_file" ]
+then
+  echo "key_vault_env_file could not be found"
+  exit 1
+fi
+
+set -a
+. "$key_vault_env_file"
+set +a
+
+db_admin_password=$(az keyvault secret show --id "$ADMIN_PASSWORD_SECRET_ID")
+db_admin_password_version=$(az keyvault secret show --id "$ADMIN_PASSWORD_VERSIN_SECRET_ID")
+
+
+cat >"$output_file" <<EOF
+db_admin_password = "$db_admin_password"
+db_admin_password_version = $db_admin_password_version
+EOF
